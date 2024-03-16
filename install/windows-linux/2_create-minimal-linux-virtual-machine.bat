2156e45c644cbf03f721e07fd9ba47d6fcb8d98c
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-16 22:18:41 +0100

    Update 2_create-minimal-linux-virtual-machine.bat

--- install/windows-linux/2_create-minimal-linux-virtual-machine.bat
+++ install/windows-linux/2_create-minimal-linux-virtual-machine.bat
@@ -2,5 +2,4 @@ mkdir ..\..\wsl-vm
 curl -L -o ubuntu-jammy.tar https://partner-images.canonical.com/oci/jammy/current/ubuntu-jammy-oci-amd64-root.tar.gz 
 wsl --import wordslab-notebooks ..\..\wsl-vm ubuntu-jammy.tar
 del ubuntu-jammy.tar
-wsl -d wordslab-notebooks -- apt update && apt install --yes wget
-wsl -d wordslab-notebooks -- wget xxx && chmod u+x xx && ./xxx && rm xxx
+wsl -d wordslab-notebooks -- ./3_install-linux-virtual-machine.sh
