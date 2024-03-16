7c6522f41b2d16cb05b28b307118411963766871
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-16 22:13:42 +0100

    Update 3_install-linux-virtual-machine.sh

--- install/windows-linux/3_install-linux-virtual-machine.sh
+++ install/windows-linux/3_install-linux-virtual-machine.sh
@@ -1,4 +1,5 @@
-apt install --yes sudo curl unzip less vim emacs tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake locales 
+apt update
+apt install --yes sudo curl wget unzip less vim emacs tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake locales 
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
 echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
