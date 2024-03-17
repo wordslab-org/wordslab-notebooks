328086f873979b24b6b1b5e6cc2fc8d2ca737b17
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-17 18:18:53 +0100

    Update 3_install_jupyterlab_workspace.sh

--- install/linux/3_install_jupyterlab_workspace.sh
+++ install/linux/3_install_jupyterlab_workspace.sh
@@ -16,3 +16,5 @@ cp ./create-workspace-project /usr/local/bin/create-workspace-project
 chmod u+x /usr/local/bin/create-workspace-project
 cp ./delete-workspace-project /usr/local/bin/delete-workspace-project
 chmod u+x /usr/local/bin/delete-workspace-project
+
+echo 'cd /workspace' >> ~/.bashrc
