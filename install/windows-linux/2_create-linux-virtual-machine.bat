b90add5f5e11b432efd4fd5f0886d8c4e54eb1b9
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-17 16:04:52 +0100

    Update and rename 2_create-minimal-linux-virtual-machine.bat to 2_create-linux-virtual-machine.bat

--- install/windows-linux/2_create-linux-virtual-machine.bat
+++ install/windows-linux/2_create-linux-virtual-machine.bat
@@ -3,3 +3,8 @@ curl -L -o ubuntu-jammy.tar https://partner-images.canonical.com/oci/jammy/curre
 wsl --import wordslab-notebooks ..\..\wsl-vm ubuntu-jammy.tar
 del ubuntu-jammy.tar
 wsl -d wordslab-notebooks -- ./3_install-linux-virtual-machine.sh
+
+cd ..\linux
+wsl -d wordslab-notebooks -- ./1_install-python-environment-manager.sh
+wsl -d wordslab-notebooks -- ./2_install-cuda-pytorch-2.2.sh
+
