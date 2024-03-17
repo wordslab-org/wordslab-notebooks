8be533998deb9b944473488bbc36e275004f25ee
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-17 14:50:46 +0100

    Update start-wordslab-notebooks.sh

--- start-wordslab-notebooks.sh
+++ start-wordslab-notebooks.sh
@@ -1,1 +1,4 @@
+eval "$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+conda activate pytorch-2.2
+
 jupyter lab -ServerApp.base_url="/" -ServerApp.port=8888 -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace"
