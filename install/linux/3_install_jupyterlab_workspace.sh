95a8652b7f59d58c2ed04339875032bfce80f7f6
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-03-17 17:12:48 +0100

    Update 3_install_jupyterlab_workspace.sh

--- install/linux/3_install_jupyterlab_workspace.sh
+++ install/linux/3_install_jupyterlab_workspace.sh
@@ -12,7 +12,7 @@ mkdir -p /workspace
 conda env config vars set JUPYTERLAB_SETTINGS_DIR=/workspace/.jupyter/lab/user-settings
 conda env config vars set JUPYTERLAB_WORKSPACES_DIR=/workspace/.jupyter/lab/workspaces
 
-cp ./create_workspace_project /usr/local/bin/create-workspace-project
+cp ./create-workspace-project /usr/local/bin/create-workspace-project
 chmod u+x /usr/local/bin/create-workspace-project
-cp ./delete_workspace_project /usr/local/bin/delete-workspace-project
+cp ./delete-workspace-project /usr/local/bin/delete-workspace-project
 chmod u+x /usr/local/bin/delete-workspace-project
