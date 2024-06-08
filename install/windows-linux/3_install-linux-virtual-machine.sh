f404113d2c30b540a1dbdf616fd0ae1585a99034
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-08 23:29:56 +0200

    Improve non interactive install and add Docker

--- install/windows-linux/3_install-linux-virtual-machine.sh
+++ install/windows-linux/3_install-linux-virtual-machine.sh
@@ -1,7 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
+export TZ="Europe/Paris"
+echo "postfix postfix/main_mailer_type string 'No configuration'" | debconf-set-selections
+
 apt update
-apt install --yes sudo curl wget unzip less vim emacs tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake locales 
+apt install --yes sudo locales ca-certificates curl wget unzip less vim emacs tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake 
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
 echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
@@ -11,3 +14,21 @@ source ~/.bashrc
 
 # Uncomment the line below if you want to generate PDF / HTML versions of your notebooks
 # apt install fonts-liberation pandoc texlive-xetex texlive-fonts-recommended texlive-plain-generic
+
+# Install Docker Engine on Ubuntu
+# https://docs.docker.com/engine/install/ubuntu/
+
+# Add Docker's official GPG key:
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install the latest version
+apt-get install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
