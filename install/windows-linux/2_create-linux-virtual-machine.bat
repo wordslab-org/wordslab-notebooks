2ce1b32e73c26d68c4eb7e8ba908d52ba3c0037b
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-08 17:33:41 +0200

    Upgrade to Ubuntu 24.04 LTS

--- install/windows-linux/2_create-linux-virtual-machine.bat
+++ install/windows-linux/2_create-linux-virtual-machine.bat
@@ -1,10 +1,10 @@
 mkdir ..\..\wsl-vm
-curl -L -o ubuntu-jammy.tar https://partner-images.canonical.com/oci/jammy/current/ubuntu-jammy-oci-amd64-root.tar.gz 
-wsl --import wordslab-notebooks ..\..\wsl-vm ubuntu-jammy.tar
-del ubuntu-jammy.tar
+curl -L -o ubuntu-noble.tar https://partner-images.canonical.com/oci/noble/current/ubuntu-noble-oci-amd64-root.tar.gz
+wsl --import wordslab-notebooks ..\..\wsl-vm ubuntu-noble.tar
+del ubuntu-noble.tar
 wsl -d wordslab-notebooks -- ./3_install-linux-virtual-machine.sh
 
 cd ..\linux
 wsl -d wordslab-notebooks -- ./1_install-python-environment-manager.sh
-wsl -d wordslab-notebooks -- ./2_install-cuda-pytorch-2.2.sh
+wsl -d wordslab-notebooks -- ./2_install-cuda-pytorch.sh
 
