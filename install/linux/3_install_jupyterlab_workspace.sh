3a7ee9cec2fead2150fcbae3e3bb6e72e318b7a6
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-08 17:51:13 +0200

    Upgrade to Jupyterlab 4.2.1

--- install/linux/3_install_jupyterlab_workspace.sh
+++ install/linux/3_install_jupyterlab_workspace.sh
@@ -1,11 +1,11 @@
 eval "$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate $1
 
-conda install -y -c conda-forge jupyterlab=4.1.5
+conda install -y -c conda-forge jupyterlab=4.2.1
 conda install -y -c conda-forge jupyterlab_execute_time=3.1.2
-conda install -y -c rapidsai-nightly -c conda-forge jupyterlab-nvdashboard=0.11.0a0
+conda install -y -c rapidsai-nightly -c conda-forge jupyterlab-nvdashboard=0.11.00
 conda install -y -c conda-forge jupyterlab-git=0.50.0
-conda install -y -c conda-forge ipympl=0.9.3
+conda install -y -c conda-forge ipympl=0.9.4
 
 mkdir -p /workspace
 
