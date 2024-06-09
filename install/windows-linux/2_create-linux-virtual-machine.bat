54cef03e5d395c5c90a22c2479ad866580e97a52
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-09 16:22:40 +0200

    Move vm installation commands to another file

--- install/windows-linux/2_create-linux-virtual-machine.bat
+++ install/windows-linux/2_create-linux-virtual-machine.bat
@@ -2,9 +2,4 @@ mkdir ..\..\wsl-vm
 curl -L -o ubuntu-noble.tar https://partner-images.canonical.com/oci/noble/current/ubuntu-noble-oci-amd64-root.tar.gz
 wsl --import wordslab-notebooks ..\..\wsl-vm ubuntu-noble.tar
 del ubuntu-noble.tar
-wsl -d wordslab-notebooks -- ./3_install-linux-virtual-machine.sh
-
-cd ..\linux
-wsl -d wordslab-notebooks -- ./1_install-python-environment-manager.sh
-wsl -d wordslab-notebooks -- ./2_install-cuda-pytorch.sh
 
