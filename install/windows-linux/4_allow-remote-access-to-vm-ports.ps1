6ae41860366c534468e31da6917cc0719812b4be
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-23 17:50:11 +0200

    Create 4_allow-remote-access-to-vm-ports.ps1

---
+++ install/windows-linux/4_allow-remote-access-to-vm-ports.ps1
@@ -0,0 +1,22 @@
+# Check if the script is running as Administrator
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "Allowing remote access to wordslab-notebooks virtual machine requires Administrator privileges. Please open a new terminal as Administrator and restart the installation script."
+    exit
+}
+
+$firewallports = ""
+for ( $i = 0; $i -lt $args.count; $i++ ) 
+{
+   $vmport = $args[$i]
+   echo "- allow remote access to VM port $vmport"
+   netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=$vmport connectaddress=127.0.0.1 connectport=$vmport
+   if ( $firewallports.Length -gt 0 ) {
+      $firewallports = $firewallports + ","
+   }
+   $firewallports = $firewallports + $vmport
+}
+if ( $firewallports.Length -gt 0 )
+{
+   echo "- configure firewall to allow inbound traffic on ports $firewallports"
+   netsh advfirewall firewall add rule name="wordslab-notabooks" protocol=TCP dir=in localport=$firewallports action=allow
+}
