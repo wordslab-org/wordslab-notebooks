3618030e6145d0e2adf4e6d87efd8166072f0ec3
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-06-23 17:57:15 +0200

    Update 4_allow-remote-access-to-vm-ports.ps1

--- install/windows-linux/4_allow-remote-access-to-vm-ports.ps1
+++ install/windows-linux/4_allow-remote-access-to-vm-ports.ps1
@@ -4,6 +4,11 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     exit
 }
 
+# Default ports : Jupyterlab, Gradio, fastapi & VLLM, argilla
+if ($args.count -eq 0) {
+    $args = @(8888, 7860, 8000, 6900)
+}
+
 $firewallports = ""
 for ( $i = 0; $i -lt $args.count; $i++ ) 
 {
