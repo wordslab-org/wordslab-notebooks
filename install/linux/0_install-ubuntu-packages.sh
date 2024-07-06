2c6dfeb3129157aaf00e4654de2feda579ae1c52
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-07-06 15:01:39 +0200

    Update 0_install-ubuntu-packages.sh
    Add ffmpeg to support audio

--- install/linux/0_install-ubuntu-packages.sh
+++ install/linux/0_install-ubuntu-packages.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 apt update
-apt install --yes sudo locales ca-certificates curl wget unzip less vim tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake 
+apt install --yes sudo locales ca-certificates curl wget unzip less vim tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake ffmpeg
 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
 echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
