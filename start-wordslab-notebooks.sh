f2d10546b3487f73ebeb977f036aa05b5a0d9a1f
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-08-11 15:25:00 +0200

    Listen on ip address 0.0.0.0 to allow remote access

--- start-wordslab-notebooks.sh
+++ start-wordslab-notebooks.sh
@@ -1,4 +1,4 @@
 eval "$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate wordslab-notebooks
 
-jupyter lab -ServerApp.base_url="/" -ServerApp.port=8888 -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace"
+jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=8888 -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace"
