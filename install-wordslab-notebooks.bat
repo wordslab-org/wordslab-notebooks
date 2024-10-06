e198b827825b5415ed9a18756dd20de9fc076905
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-06 11:57:18 +0200

    Update install-wordslab-notebooks.bat
    Manage the machine reboot required after wsl installation

--- install-wordslab-notebooks.bat
+++ install-wordslab-notebooks.bat
@@ -1,5 +1,11 @@
 @echo off
 cd .\install\windows-linux
+
 call 1_install-or-update-windows-subsystem-for-linux.bat
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 call 2_create-linux-virtual-machine.bat
 call 3_install-linux-virtual-machine.bat
+
+@echo off
+cd .\
