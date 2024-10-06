585e539ab3b63f2980e083120eea4f96187fb65c
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-06 17:11:18 +0200

    Update start-wordslab-notebooks.sh
    Fix miniforge path

--- start-wordslab-notebooks.sh
+++ start-wordslab-notebooks.sh
@@ -1,4 +1,4 @@
-eval "$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+eval "$('/root/miniforge3/condabin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate pytorch-2.4
 
 jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=8888 -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace"
