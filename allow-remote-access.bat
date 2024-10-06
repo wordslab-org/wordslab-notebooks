ebac4afb5bd283620d6b0cb7c12a6641f6524a44
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-06 16:19:36 +0200

    Add elevation of privileges

--- allow-remote-access.bat
+++ allow-remote-access.bat
@@ -1,10 +1,12 @@
 @echo off
+echo %~f0
 net session >nul 2>&1
-if %errorLevel% != 0 (
+if %errorLevel% GTR 0 (
     echo Requesting administrator privileges...
     PowerShell -Command "Start-Process '%~f0' -Verb RunAs"
     exit /B
 )
 
-cd install\windows
-4_allow-remote-access-to-vm-ports.bat %*
+PowerShell -ExecutionPolicy Bypass -File %~dp0install\windows\4_allow-remote-access-to-vm-ports.ps1 %*
+
+pause
