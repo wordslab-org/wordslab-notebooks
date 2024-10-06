aa26ed87a27c7c04e56029fd7e5df67eedd17a71
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-06 12:31:23 +0200

    Update allow-remote-access.bat
    Include elevation of privileges

--- allow-remote-access.bat
+++ allow-remote-access.bat
@@ -1,3 +1,10 @@
 @echo off
+net session >nul 2>&1
+if %errorLevel% != 0 (
+    echo Requesting administrator privileges...
+    PowerShell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /B
+)
+
 cd install\windows
 4_allow-remote-access-to-vm-ports.bat %*
