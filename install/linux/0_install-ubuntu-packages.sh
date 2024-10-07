2f2a809e105cf5323190c39cf3786a5ed0691eab
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-08 00:34:11 +0200

    Add nvidia container runtime for Docker

--- install/linux/0_install-ubuntu-packages.sh
+++ install/linux/0_install-ubuntu-packages.sh
@@ -20,12 +20,25 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add Docker repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-# Install the latest version
+# Install Docker latest version
 apt-get install --yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add nvidia repository to Apt sources:
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# Install nvidia container runtime
+apt-get update
+apt-get install -y nvidia-container-toolkit
+
+# Configure Docker to use nvidia container runtime
+nvidia-ctk runtime configure --runtime=docker
