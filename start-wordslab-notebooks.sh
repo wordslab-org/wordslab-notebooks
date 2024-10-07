569fad2d7c9f439d1d70501a000d93544466715b
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-10-08 00:47:18 +0200

    Start docker before jupyterlab

--- start-wordslab-notebooks.sh
+++ start-wordslab-notebooks.sh
@@ -1,4 +1,9 @@
 eval "$('/root/miniforge3/condabin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate pytorch-2.4
 
+if ! pgrep -x "dockerd" > /dev/null; then
+    echo "Docker is not running. Starting Docker service..."
+    service docker start
+fi
+
 jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=8888 -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace"
