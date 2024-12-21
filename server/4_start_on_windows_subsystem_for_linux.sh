b48d5b9bae169f3c7816118bbdff3e651175bed0
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-21 16:36:37 +0100

    Fix workspace dir

--- server/4_start_on_windows_subsystem_for_linux.sh
+++ server/4_start_on_windows_subsystem_for_linux.sh
@@ -5,7 +5,7 @@ fi
 code-server --auth none --bind-addr 0.0.0.0 --port $VSCODE_PORT --user-data-dir $WORKSPACE_DIR/.code-server --extensions-dir $WORKSPACE_DIR/.code-server/extensions --config $WORKSPACE_DIR/.code-server/config.yaml $WORKSPACE_DIR &
 pid1=$!
 
-jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=$JUPYTERLAB_PORT -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="/workspace" &
+jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=$JUPYTERLAB_PORT -IdentityProvider.token="" --no-browser -ServerApp.allow_root=True -ServerApp.allow_remote_access=True -ServerApp.root_dir="$WORKSPACE_DIR" &
 pid2=$!
 
 # Define cleanup function to kill both commands
