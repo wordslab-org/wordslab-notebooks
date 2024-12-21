824975dd1ef9a6572890e8882e2d3154643432f7
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-21 16:25:33 +0100

    Upgrade to pytorch 2.5.1

--- server/2_2_install-pytorch-cpu.sh
+++ server/2_2_install-pytorch-cpu.sh
@@ -1,8 +1,8 @@
 eval "$('/root/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 
-conda create -y --name pytorch-2.4 python==3.12.7 ninja=1.12.1
-conda activate pytorch-2.4
+conda create -y --name pytorch-2.5 python==3.12.8 ninja=1.12.1
+conda activate pytorch-2.5
 
-conda install -y pytorch=2.4.0 torchvision=0.19.0 torchaudio=2.4.0 cpuonly -c pytorch
+pip install torch=2.5.1 torchvision=0.20.1 torchaudio=2.5.1 --index-url https://download.pytorch.org/whl/cpu
 
-echo 'conda activate pytorch-2.4' >> ~/.bashrc
+echo 'conda activate pytorch-2.5' >> ~/.bashrc
