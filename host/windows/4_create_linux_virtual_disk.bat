896f26aabf256702db850157601d2156fc6ae1d0
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-21 16:11:50 +0100

    Update 4_create_linux_virtual_disk.bat
    Update alpine version

--- host/windows/4_create_linux_virtual_disk.bat
+++ host/windows/4_create_linux_virtual_disk.bat
@@ -1,7 +1,7 @@
 REM Mandatory parameters : 3_create_linux_virtual_disk.bat <windows directory for shared models disk>
 
 mkdir %~1\wordslab-notebooks-models
-curl -L -o alpine.tar https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.3-x86_64.tar.gz
+curl -L -o alpine.tar https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-minirootfs-3.21.0-x86_64.tar.gz
 wsl --import wordslab-notebooks-models %~1\wordslab-notebooks-models alpine.tar
 del alpine.tar
 
