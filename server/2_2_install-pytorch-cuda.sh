d264b59def0880305fdc130401bd3f1f3cf824d4
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 21:36:52 +0100

    Update conda env name

--- server/2_2_install-pytorch-cuda.sh
+++ server/2_2_install-pytorch-cuda.sh
@@ -1,8 +1,8 @@
 eval "$('/root/miniforge3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 
-conda create -y --name pytorch-2.5 python==3.12.8 ninja=1.12.1
-conda activate pytorch-2.5
+conda create -y --name wordslab-2024-12 python==3.12.8 ninja=1.12.1
+conda activate wordslab-2024-12
 
 pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1
 
-echo 'conda activate pytorch-2.5' >> ~/.bashrc
+echo 'conda activate wordslab-2024-12' >> ~/.bashrc
