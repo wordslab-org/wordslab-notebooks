1b66d9bb13b647e7a9dc9433797561b0b2175968
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 21:50:40 +0100

    Start open-webui

--- server/4_start_on_windows_subsystem_for_linux.sh
+++ server/4_start_on_windows_subsystem_for_linux.sh
@@ -11,12 +11,17 @@ pid2=$!
 OLLAMA_HOME=0.0.0.0  $WORKSPACE_DIR/ollama/bin/ollama serve &
 pid3=$!
 
+conda activate openwebui-2024-12
+ENV=prod WEBUI_AUTH=false WEBUI_URL=http://localhost:8080 DATA_DIR=$WORKSPACE_DIR/open-webui FUNCTIONS_DIR=$WORKSPACE_DIR/open-webui/functions TOOLS_DIR=$WORKSPACE_DIR/open-webui/tools open-webui serve --host 0.0.0.0 --port 8080 &
+pid4=$!
+conda deactivate
+
 # Define cleanup function to kill all commands
 cleanup() {
   echo "Stopping commands..."
-  kill $pid1 $pid2 $pid3
+  kill $pid1 $pid2 $pid3 $pid4
 }
 # Trap SIGINT and call cleanup
 trap cleanup SIGINT
 # Wait for all processes to finish
-wait $pid1 $pid2 $pid3
+wait $pid1 $pid2 $pid3 $pid4
