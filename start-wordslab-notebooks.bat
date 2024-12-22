7da1cf0b4e5bfb1da74ab0dc5d5ad14995dd49bb
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 15:55:09 +0100

    Update start-wordslab-notebooks.bat

--- start-wordslab-notebooks.bat
+++ start-wordslab-notebooks.bat
@@ -15,7 +15,7 @@ REM Check if a separate disk was initialized to store the models
 wsl -d wordslab-notebooks-models -- : >nul
 if %errorlevel% equ 0 (
     wsl -d wordslab-notebooks-models -- mkdir -p /mnt/wsl/wordslab-notebooks-models
-    wsl -d wordslab-notebooks-models -- mount --bind /data /mnt/wsl/wordslab-notebooks-models
+    wsl -d wordslab-notebooks-models -- mount --bind /models /mnt/wsl/wordslab-notebooks-models
     wsl -d %name% -- mount --bind /mnt/wsl/wordslab-notebooks-models /models
 )
 
