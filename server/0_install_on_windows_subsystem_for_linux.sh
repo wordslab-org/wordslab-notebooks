6e75f6c2d3e71ee66b5ce0458b0a2a6f752e1f73
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 22:33:41 +0100

    Rename install-ollama

--- server/0_install_on_windows_subsystem_for_linux.sh
+++ server/0_install_on_windows_subsystem_for_linux.sh
@@ -25,7 +25,7 @@ source ~/wordslab-notebooks-environment.sh
 ./3_2_configure_jupyterlab_git_support.sh
 ./3_3_configure_jupyterlab_resources_monitoring.sh
 ./3_4_install_vscode_server.sh
-./3_5_install-ollama-openwebui.sh
+./3_5_install-ollama.sh
 ./3_6_install-open-webui.sh
 
 echo 'source ~/wordslab-notebooks-environment.sh' >> ~/.bashrc
