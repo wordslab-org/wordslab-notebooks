612c06f7eaa78a2486d0bcf4abb3a310639bfcd2
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 22:28:32 +0100

    Return to original  dir after install

--- host/windows/3_install-linux-virtual-machine.bat
+++ host/windows/3_install-linux-virtual-machine.bat
@@ -1,5 +1,10 @@
 REM Usage : 2_create-linux-virtual-machine.bat <wsl distribution name>(required) <cpu only?>(required)
 
-cd ..\..\server
+rem Capture the current directory
+set "originalDir=%cd%"
 
+cd ..\..\server
 wsl -d %1 -- ./0_install_on_windows_subsystem_for_linux.sh %2
+
+rem Return to the original directory
+cd "%originalDir%"
