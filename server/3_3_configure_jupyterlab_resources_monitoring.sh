f41ec33b75e4007663cdc2547da104a7615db1cb
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 14:37:50 +0100

    Update 3_3_configure_jupyterlab_resources_monitoring.sh

--- server/3_3_configure_jupyterlab_resources_monitoring.sh
+++ server/3_3_configure_jupyterlab_resources_monitoring.sh
@@ -1,3 +1,3 @@
-pip install jupyterlab_execute_time=3.2.0
-pip install jupyterlab-nvdashboard=0.11.00
-pip install ipympl=0.9.5
+pip install jupyterlab_execute_time==3.2.0
+pip install jupyterlab-nvdashboard==0.11.00
+pip install ipympl==0.9.5
