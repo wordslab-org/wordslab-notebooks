fdaf4dde615fd7d9677d62c28f47961d8324ee72
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 15:28:34 +0100

    Update 3_5_install-ollama-openwebui.sh

--- server/3_5_install-ollama-openwebui.sh
+++ server/3_5_install-ollama-openwebui.sh
@@ -2,3 +2,5 @@ mkdir $WORKSPACE_DIR/ollama
 curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
 tar -C $WORKSPACE_DIR/ollama -xzf ollama-linux-amd64.tgz
 rm ollama-linux-amd64.tgz
+
+echo 'export PATH="$WORKSPACE_DIR/ollama/bin:$PATH"' >> ~/wordslab-notebooks-environment.sh
