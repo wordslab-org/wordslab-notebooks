6fef3c8a4656ce861d282e1559d47d9e200e9f8f
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2024-12-22 14:47:44 +0100

    Update 3_5_install-ollama-openwebui.sh

--- server/3_5_install-ollama-openwebui.sh
+++ server/3_5_install-ollama-openwebui.sh
@@ -1,4 +1,4 @@
 mkdir $WORKSPACE_DIR/ollama
 curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
 tar -C $WORKSPACE_DIR/ollama -xzf ollama-linux-amd64.tgz
-del ollama-linux-amd64.tgz
+rm ollama-linux-amd64.tgz
