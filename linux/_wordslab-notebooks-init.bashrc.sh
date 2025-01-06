8aecb8df7758af687d30890a5bad47c7b35b7740
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-06 22:33:17 +0100

    Fix CONDA hook

--- linux/_wordslab-notebooks-init.bashrc.sh
+++ linux/_wordslab-notebooks-init.bashrc.sh
@@ -1,5 +1,6 @@
 # Source this file in ~/.bashrc to configure the machine shell
 
 # Start in the right Python environment and in the right directory 
+source <("$CONDA_DIR/bin/conda" 'shell.bash' 'hook' 2> /dev/null)
 conda activate $WORDSLAB_NOTEBOOKS_ENV
 cd $WORDSLAB_WORKSPACE
