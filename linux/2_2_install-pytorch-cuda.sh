0df0ad3b168ef2772c65844a0e8ac4b255ed22d3
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-06 22:29:54 +0100

    Fix CONDA hook

--- linux/2_2_install-pytorch-cuda.sh
+++ linux/2_2_install-pytorch-cuda.sh
@@ -3,7 +3,7 @@
 # Create a dedicated conda environment to install the CUDA GPU version of Pytorch
 # The files are stored in an independent and persistent directory: $WORDSLAB_NOTEBOOKS_ENV (see _wordslab-notebooks-env.bashrc)
 
-eval "$('$CONDA_DIR/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+source <("$CONDA_DIR/bin/conda" 'shell.bash' 'hook' 2> /dev/null)
 
 conda create -y --prefix $WORDSLAB_NOTEBOOKS_ENV python==3.12.8 ninja=1.12.1 ipykernel=6.29.5
 conda activate $WORDSLAB_NOTEBOOKS_ENV
