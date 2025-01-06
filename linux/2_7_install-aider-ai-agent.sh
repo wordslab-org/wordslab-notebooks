91b369e78e0f1b4bfdbfc12c8dae4c550e84d07c
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-07 00:11:13 +0100

    Set aider install directory

--- linux/2_7_install-aider-ai-agent.sh
+++ linux/2_7_install-aider-ai-agent.sh
@@ -3,5 +3,5 @@
 # Install uv package manager, which is required to install aider
 pip install uv  
 
-# Install Aider latest version
-uv tool install --force --python python3.12 aider-chat@latest
+# Install Aider specific version
+UV_TOOL_DIR=$WORDSLAB_NOTEBOOKS_ENV/uv/tools UV_TOOL_BIN_DIR=$WORDSLAB_NOTEBOOKS_ENV/bin uv tool install --force --python python3.12 aider-chat==0.70.0
