09334c73ba4ce03e19763d7590ab49d0c0660d9e
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-06 21:28:42 +0100

    Update install directory

--- windows/3_install-linux-virtual-machine.bat
+++ windows/3_install-linux-virtual-machine.bat
@@ -3,8 +3,8 @@ REM Usage : 2_create-linux-virtual-machine.bat <wsl distribution name>(required)
 rem Capture the current directory
 set "originalDir=%cd%"
 
-cd ..\..\server
-wsl -d %1 -- ./0_install_on_windows_subsystem_for_linux.sh %2
+cd ..
+wsl -d %1 -- ./install-wordslab-notebooks.sh %2
 
 rem Return to the original directory
 cd "%originalDir%"
