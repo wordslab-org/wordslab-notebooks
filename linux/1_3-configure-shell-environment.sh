7fb987a00cdf35ee28167c0bdea2ef0ee68903fb
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-06 22:35:01 +0100

    Move CONDA hook in _wordslab-notebooks-init.bashrc

--- linux/1_3-configure-shell-environment.sh
+++ linux/1_3-configure-shell-environment.sh
@@ -2,10 +2,6 @@
 
 # NB: this script can only be run after 2__install_wordslab-notebooks.sh AND 3__install-open-webui.sh
 
-# Insert conda initialization code in ~/.bashrc
-eval "$('$CONDA_DIR/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-conda init bash
-
 # Source _wordslab-notebooks.bashrc in ~/.bashrc
 # - set environment variables for storage and ports
 # - start a new shell in the right Python environment and the right directory
