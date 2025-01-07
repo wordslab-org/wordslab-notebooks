8fe012b3793354c7c5ce7c9a622d0ed38366a0f1
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-07 01:01:30 +0100

    Fix models directory

--- start-wordslab-notebooks.bat
+++ start-wordslab-notebooks.bat
@@ -15,8 +15,8 @@ REM Check if a separate disk was initialized to store the models
 wsl -d wordslab-notebooks-models -- : >nul
 if %errorlevel% equ 0 (
     wsl -d wordslab-notebooks-models -- mkdir -p /mnt/wsl/wordslab-notebooks-models
-    wsl -d wordslab-notebooks-models -- mount --bind /models /mnt/wsl/wordslab-notebooks-models
-    wsl -d %name% -- mount --bind /mnt/wsl/wordslab-notebooks-models /models
+    wsl -d wordslab-notebooks-models -- mount --bind /home/models /mnt/wsl/wordslab-notebooks-models
+    wsl -d %name% -- mount --bind /mnt/wsl/wordslab-notebooks-models /home/models
 )
 
 wsl -d %name% -- bash -i ~/start-wordslab-notebooks.sh
