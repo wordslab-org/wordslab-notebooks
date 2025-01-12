bdeff20c2977d4223ab8416580114f13611bedf8
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-12 15:13:27 +0100

    Add comment for the future in 5_install-linux-virtual-machine.bat

--- windows/5_install-linux-virtual-machine.bat
+++ windows/5_install-linux-virtual-machine.bat
@@ -3,6 +3,9 @@ REM Usage : 5_install-linux-virtual-machine <wsl distribution name>(required) <c
 rem Capture the current directory
 set "originalDir=%cd%"
 
+REM Note : you can't change WORDSLAB_WORKSPACE or WORDSLAB_MODELS default paths in the WSL virtual machine
+REM => this would break 4_mount-linux-virtual-disks.bat
+
 cd ..
 wsl -d %1 -- ./install-wordslab-notebooks.sh %2
 
