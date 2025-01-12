0111494bfc1c7122bbb39a79f2c42c63d15ca0b5
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-12 15:10:49 +0100

    Mount workspace and models virtual disks

---
+++ windows/4_mount-linux-virtual-disks.bat
@@ -0,0 +1,9 @@
+REM Mandatory parameter : 4_mount-linux-virtual-disks.bat <wsl distribution name>
+
+wsl -d wordslab-notebooks-workspace -- mkdir -p /mnt/wsl/wordslab-notebooks-workspace
+wsl -d wordslab-notebooks-workspace -- mount --bind /home/workspace /mnt/wsl/wordslab-notebooks-workspace
+wsl -d %name% -- mount --bind /mnt/wsl/wordslab-notebooks-workspace /home/workspace
+
+wsl -d wordslab-notebooks-models -- mkdir -p /mnt/wsl/wordslab-notebooks-models
+wsl -d wordslab-notebooks-models -- mount --bind /home/models /mnt/wsl/wordslab-notebooks-models
+wsl -d %name% -- mount --bind /mnt/wsl/wordslab-notebooks-models /home/models
