9fe3091da5c59f31983db828fee66f459980fa82
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-12 14:12:55 +0100

    Support multiple vms in the same directory

--- windows/2_create-linux-virtual-machine.bat
+++ windows/2_create-linux-virtual-machine.bat
@@ -1,7 +1,7 @@
 REM Mandatory parameter : 2_create-linux-virtual-machine.bat <wsl distribution name>
 
-mkdir ..\disks
+mkdir ..\virtual-machines\%1
 curl -L -o ubuntu-noble.tar https://partner-images.canonical.com/oci/noble/current/ubuntu-noble-oci-amd64-root.tar.gz
-wsl --import %1 ..\disks ubuntu-noble.tar
+wsl --import %1 ..\virtual-machines\%1 ubuntu-noble.tar
 del ubuntu-noble.tar
 
