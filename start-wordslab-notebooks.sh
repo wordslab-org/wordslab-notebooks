62ff0f81231934265ad399169f7ac82e2369eb2a
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-13 23:02:55 +0100

    Final fix of directories

--- start-wordslab-notebooks.sh
+++ start-wordslab-notebooks.sh
@@ -9,12 +9,12 @@ if [ -z "${WORDSLAB_HOME}" ]; then
     export $WORDSLAB_HOME=/home
 fi
 
-# Navigate to the linux directory where all the scripts live
-cd $WORDSLAB_HOME/wordslab-notebooks/linux
-
 # If the container root file system was reset after we restart an instance
 # we need to reinstall the operating system packages and re configure the shell
 if [ ! -f "/.wordslab-$WORDSLAB_VERSION-installed" ]; then
+    # Navigate to the linux directory where all the scripts live
+    cd $WORDSLAB_HOME/wordslab-notebooks/linux
+
     # Set the initial environment variables
     source ./_wordslab-notebooks-env.bashrc
 
@@ -22,8 +22,12 @@ if [ ! -f "/.wordslab-$WORDSLAB_VERSION-installed" ]; then
     ./1__update-operating-system.sh
 fi
 
-# Initialize the Python environment
+# Initialize the Python environment 
+# (this will also change directory to $WORDSLAB_WORKSPACE)
 source ~/.bashrc
+
+# Navigate (again) to the linux directory where all the scripts live
+cd $WORDSLAB_HOME/wordslab-notebooks/linux
 
 # Start all the wordslab-notebooks servers
 ./4_start-wordslab-notebooks.sh
