c1dbd85ed990a57bd3069f0e65ca293dd8ed3bf9
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-01-14 23:50:43 +0100

    Fix Open WebUI installation order

--- linux/3_2_install-open-webui.sh
+++ linux/3_2_install-open-webui.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Create the directory inside the workspace where all Open WebUI state will be stored
+
+mkdir $OPENWEBUI_DATA
+mkdir $OPENWEBUI_DATA/functions
+mkdir $OPENWEBUI_DATA/tools
+
 # Create a dedicated conda environment to install Open WebUI and its dependencies
 # The files are stored in an independent and persistent directory: $WORDSLAB_NOTEBOOKS_ENV (see _wordslab-notebooks-env.bashrc)
 
@@ -10,14 +16,8 @@ conda activate $OPENWEBUI_ENV
 
 pip install open-webui==0.5.4
 
-conda deactivate
-
-# Create the directory inside the workspace where all Open WebUI state will be stored
-
-mkdir $OPENWEBUI_DATA
-mkdir $OPENWEBUI_DATA/functions
-mkdir $OPENWEBUI_DATA/tools
-
 # Initialize the Open WebUI installation
 
 DATA_DIR=$OPENWEBUI_DATA FUNCTIONS_DIR=$OPENWEBUI_DATA/functions TOOLS_DIR=$OPENWEBUI_DATA/tools python -c "import open_webui.main"
+
+conda deactivate
