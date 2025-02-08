9f256de485c414118c6a2d8b8ca29d89fa6e8088
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-02-08 21:24:32 +0100

    Upgrade to openwebui 0.5.10 to get code interpreter

--- linux/3_2_install-open-webui.sh
+++ linux/3_2_install-open-webui.sh
@@ -14,7 +14,7 @@ source <("$CONDA_DIR/bin/conda" 'shell.bash' 'hook' 2> /dev/null)
 conda create -y --prefix $OPENWEBUI_ENV python==3.12.8
 conda activate $OPENWEBUI_ENV
 
-pip install open-webui==0.5.4
+pip install open-webui==0.5.10
 
 # Initialize the Open WebUI installation
 
