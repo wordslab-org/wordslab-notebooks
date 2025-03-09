27e6995e6c0d92e18a68701ab727c1811d8d0603
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-09 09:13:57 +0100

    VsCode extensions latest version

--- linux/2_6_install-vscode-server.sh
+++ linux/2_6_install-vscode-server.sh
@@ -7,7 +7,7 @@ curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.97.2 --meth
 mkdir $VSCODE_DATA
 
 # Install the Microsoft Python extension
-$VSCODE_DIR/bin/code-server --install-extension ms-python.python@2025.0.0 --extensions-dir $VSCODE_DATA/extensions
+$VSCODE_DIR/bin/code-server --install-extension ms-python.python@2025.2.0 --extensions-dir $VSCODE_DATA/extensions
 
 # Install the Continue AI code assistant
-CONTINUE_GLOBAL_DIR=$VSCODE_DATA/.continue $VSCODE_DIR/bin/code-server --install-extension Continue.continue@1.0.1 --extensions-dir $VSCODE_DATA/extensions
+CONTINUE_GLOBAL_DIR=$VSCODE_DATA/.continue $VSCODE_DIR/bin/code-server --install-extension Continue.continue@1.0.3 --extensions-dir $VSCODE_DATA/extensions
