d8dd3f340a72e2085b584840e8adb436794b1b28
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-22 14:39:04 +0100

    Upgrade to pytorch 2.6.0 CPU

--- linux/2_2_install-pytorch-cpu.sh
+++ linux/2_2_install-pytorch-cpu.sh
@@ -2,7 +2,7 @@
 
 # Install the CPU version of Pytorch
 
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cpu
 
 # Register that the install is CPU only
 touch $WORDSLAB_NOTEBOOKS_ENV/.cpu-only
