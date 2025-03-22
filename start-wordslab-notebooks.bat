bec7881463e60dd16159dc3eebcabc046a4d3018
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-22 18:48:43 +0100

    Fix administrator check and request in start-wordslab-notebooks.bat

--- start-wordslab-notebooks.bat
+++ start-wordslab-notebooks.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 REM Usage : start-wordslab-notebooks.bat --name <wsl distribution name>(optional)
 REM Default parameter values
@@ -20,15 +21,16 @@ REM => launch wordslab-notebooks in "remote access" mode
 REM => allow remote access to ports 8880-8888 of the WSL virtual machine
 REM Note: we need to do this each time we start the VM, because its IP address changes each time you launch WSL
 wsl -d wordslab-notebooks-workspace -- sh -c "if [ -f /home/workspace/.secrets/certificate.pem ]; then exit 0; else exit 1; fi"
-if %errorlevel% equ 0 (
-    echo Launching wordslab-notebooks in "remote access" mode: opening and forwarding ports 8880-8888 to the virtual machine
+if !ERRORLEVEL! equ 0 (
+    echo Launching wordslab-notebooks in "remote access" mode
+    echo - opening and forwarding ports 8880-8888 to the virtual machine
     net session >nul 2>&1
-    if %errorLevel% GTR 0 (
-        echo Requesting administrator privileges...
-        PowerShell -Command "Start-Process '%~f0' -Verb RunAs"
-        exit /B
+    if !ERRORLEVEL! equ 0 (
+        PowerShell -ExecutionPolicy Bypass -File 6_allow-remote-access-to-vm-ports.ps1 %*
+    ) else (
+        echo - requesting administrator privileges to launch the script 6_allow-remote-access-to-vm-ports.ps1
+        PowerShell -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File %CD%\6_allow-remote-access-to-vm-ports.ps1 %*' -Verb RunAs"
     )
-    PowerShell -ExecutionPolicy Bypass -File 6_allow-remote-access-to-vm-ports.ps1 %*
 )
 
 cd ..
