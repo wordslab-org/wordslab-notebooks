4caf06f0965c5ec8025983a2e23da4dfa7d96767
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-23 10:17:29 +0100

    Add the Windows machine IP in prepare-server-machine.bat

--- prepare-server-machine.bat
+++ prepare-server-machine.bat
@@ -1,6 +1,13 @@
 @echo off
+
+REM Get the IP address of the Windows server machine (first non WSL and non loopback IP address)
+for /f %%I in ('powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-NetIPAddress -AddressFamily IPv4 -AddressState Preferred | Where-Object {($_.InterfaceAlias -notmatch 'WSL') -and ($_.InterfaceAlias -notmatch 'Loopback')} | Select-Object -ExpandProperty IPAddress"') do set IP=%%I
+
+REM Write the IP address to a file in the WSL /home directory
+wsl -d wordslab-notebooks -- bash -c "echo %IP% > /home/.WORDSLAB_WINDOWS_IP"
+
 set "tarfile="
-for %%f in ("..\secrets\wordslab-server-*.tar") do (
+for %%f in ("..\secrets\wordslab-server-%IP%-secrets.tar") do (
     set "tarfile=%%~nxf"
 )
 
@@ -8,5 +15,5 @@ if defined tarfile (
     echo Installing secrets files on the server machine:
     wsl -d wordslab-notebooks-workspace -- sh -c "mkdir -p /home/workspace/.secrets && cp ../secrets/%tarfile% /home/workspace/.secrets && cd /home/workspace/.secrets && tar -xvf %tarfile%"
 ) else (
-    echo You first need to copy the .tar file of the secrets generated on the client machine to the ..\secrets directory.
+    echo You first need to copy the wordslab-server-%IP%-secrets.tar file of the secrets generated on the client machine to the ..\secrets directory.
 )
