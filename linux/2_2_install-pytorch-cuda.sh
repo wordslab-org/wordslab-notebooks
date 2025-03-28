1e4f5fd0acc42007a93255d72ee621a48af6aff8
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-28 22:40:10 +0100

    Add vllm to the Pytorch GPU install

--- linux/2_2_install-pytorch-cuda.sh
+++ linux/2_2_install-pytorch-cuda.sh
@@ -3,3 +3,7 @@
 # Install the CUDA GPU version of Pytorch
 
 pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0
+
+# Install vllm here because it will now be closely linked to a version of Pytorch
+
+pip install vllm==0.8.2
