8902e383cab413d34f4ace6813c6a773d6eb3f9d
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-03-28 23:56:30 +0100

    Fix errors in dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,14 +3,16 @@ import os
 
 version = os.getenv("WORDSLAB_VERSION")
 
-jupyterlab_url = int(os.getenv("JUPYTERLAB_URL"))
-vscode_url = int(os.getenv("VSCODE_URL"))
-openwebui_url = int(os.getenv("OPENWEBUI_URL"))
-app1_url = int(os.getenv("USER_APP1_URL"))
-app2_url = int(os.getenv("USER_APP2_URL"))
-app3_url = int(os.getenv("USER_APP3_URL"))
-app4_url = int(os.getenv("USER_APP2_URL"))
-app5_url = int(os.getenv("USER_APP3_URL"))
+dashboard_port=int(os.getenv("DASHBOARD_PORT"))
+
+jupyterlab_url = os.getenv("JUPYTERLAB_URL")
+vscode_url = os.getenv("VSCODE_URL")
+openwebui_url = os.getenv("OPENWEBUI_URL")
+app1_url = os.getenv("USER_APP1_URL")
+app2_url = os.getenv("USER_APP2_URL")
+app3_url = os.getenv("USER_APP3_URL")
+app4_url = os.getenv("USER_APP2_URL")
+app5_url = os.getenv("USER_APP3_URL")
 
 app,rt = fast_app()
 
@@ -23,15 +25,15 @@ def get(): return Titled(f"wordslab notebooks {version}",
                              ToolLink("Open WebUI", openwebui_url)),
                          H3("User apps"),
                          Ul(
-                             ToolLink("User application 1", app1_port),
-                             ToolLink("User application 2", app2_port),
-                             ToolLink("User application 3", app3_port),
-                             ToolLink("User application 4", app4_port),
-                             ToolLink("User application 5", app5_port)
+                             ToolLink("User application 1", app1_url),
+                             ToolLink("User application 2", app2_url),
+                             ToolLink("User application 3", app3_url),
+                             ToolLink("User application 4", app4_url),
+                             ToolLink("User application 5", app5_url)
                          )
                         )
 
 def ToolLink(name, url):
-    return Li(name + ": ", A(url, href=url, target="_blank"))  
+    return Li(name + ": ", A(url, href=url, target="_blank"))
 
 serve(port=dashboard_port)
