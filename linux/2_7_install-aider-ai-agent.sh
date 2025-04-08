20384c317837ff7b1438c5f23274357e1530efe7
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-04-08 22:08:23 +0200

    update aider version

--- linux/2_7_install-aider-ai-agent.sh
+++ linux/2_7_install-aider-ai-agent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install Aider - AI pair programming in your terminal
-UV_TOOL_DIR=$WORDSLAB_NOTEBOOKS_ENV/uv/tools UV_TOOL_BIN_DIR=$WORDSLAB_NOTEBOOKS_ENV/bin uv tool install --force --python python3.12 aider-chat==0.80.0
+UV_TOOL_DIR=$WORDSLAB_NOTEBOOKS_ENV/uv/tools UV_TOOL_BIN_DIR=$WORDSLAB_NOTEBOOKS_ENV/bin uv tool install --force --python python3.12 aider-chat==0.81.0
