5879fc9c2648126b6650d175fcdd30f208ba15d8
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-04-08 22:06:26 +0200

    update all versions

--- linux/2_4_install-jupyterlab.sh
+++ linux/2_4_install-jupyterlab.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Install Jupyterlab and its dependencies
-pip install jupyterlab==4.3.6 ipympl==0.9.7
+pip install jupyterlab==4.4.0 ipympl==0.9.7
 
 # Jupyterlab UI extension for git source code versioning
-pip install jupyterlab-git==0.51.0
+pip install jupyterlab-git==0.51.1
 
 # Jupyterlab UI extensions for performance and compute resources monitoring
 pip install jupyterlab_execute_time==3.2.0
 pip install jupyterlab-nvdashboard==0.13.0 pynvml==12.0.0
 
 # Install nbdev to enable the development of Python packages and documentation from Jupyter notebooks
-pip install nbdev==2.3.34
+pip install nbdev==2.4.2
 
 # Install AI extension for Jupyter notebooks
-pip install jupyter-ai==2.31.0 langchain-ollama==0.3.0 langchain-openai==0.3.9
+pip install jupyter-ai==2.31.2 langchain-ollama==0.3.1 langchain-openai==0.3.12
 
 # Configure Jupyterlab to store all its state under the $WORDSLAB_WORKSPACE directory (see _wordslab-notebooks-env.bashrc)
 
