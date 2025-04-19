3b3ad93f1cd4bff5650729993658e47f1cde726a
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-04-19 22:22:09 +0200

    Upgrade miniforge version

--- linux/2_1_install-python-environment.sh
+++ linux/2_1_install-python-environment.sh
@@ -3,7 +3,7 @@
 # Install the latest release of Miniforge : minimal installers for Conda and Mamba specific to conda-forge
 # The files are stored in an independent and persistent directory: $CONDA_DIR (see _wordslab-notebooks-env.bashrc)
 
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+curl -L -O "https://github.com/conda-forge/miniforge/releases/download/25.3.0-1/Miniforge3-Linux-x86_64.sh"
 
 bash Miniforge3-Linux-x86_64.sh -b -p $CONDA_DIR
 
