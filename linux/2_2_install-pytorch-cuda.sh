1bc8c5928335c8393deffa47acd2a3ec08d654f9
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-04-19 21:11:11 +0200

    Upgrade vllm version

--- linux/2_2_install-pytorch-cuda.sh
+++ linux/2_2_install-pytorch-cuda.sh
@@ -6,4 +6,4 @@ pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0
 
 # Install vllm here because it will now be closely linked to a version of Pytorch
 
-pip install vllm==0.8.3
+pip install vllm==0.8.4
