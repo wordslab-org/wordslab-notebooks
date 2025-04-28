e62a034bf8edfc3951fdcebd202903d2aee28413
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-04-28 23:48:28 +0200

    Fix uv env variables

--- linux/2_1_install-python-environment.sh
+++ linux/2_1_install-python-environment.sh
@@ -3,7 +3,7 @@
 # Donwload and install uv, the modern Python package manager
 mkdir -p $UV_INSTALL_DIR
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source ~/.bashrc
+source $UV_INSTALL_DIR/env
 
 # Install Python version
 uv python install 3.12.10
