8f7e72740a8eb59c374d7b9b38a64948da6034bb
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-05-03 19:19:56 +0200

    First styling of the dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,4 +1,5 @@
 from fasthtml.common import *
+from monsterui.all import *
 import os
 
 version = os.getenv("WORDSLAB_VERSION")
@@ -14,26 +15,47 @@ app3_url = os.getenv("USER_APP3_URL")
 app4_url = os.getenv("USER_APP2_URL")
 app5_url = os.getenv("USER_APP3_URL")
 
-app,rt = fast_app()
+# Create your app with the theme
+hdrs = Theme.blue.headers()
+app, rt = fast_app(hdrs=hdrs, static_path="images", debug=True, live=True)
 
 @rt("/")
-def get(): return Titled(f"wordslab notebooks {version}",
-                         H3("Main tools"),
-                         Ul(
-                             ToolLink("JupyterLab", jupyterlab_url),
-                             ToolLink("Visual Studio Code", vscode_url),
-                             ToolLink("Open WebUI", openwebui_url)),
-                         H3("User apps"),
-                         Ul(
-                             ToolLink("User application 1", app1_url),
-                             ToolLink("User application 2", app2_url),
-                             ToolLink("User application 3", app3_url),
-                             ToolLink("User application 4", app4_url),
-                             ToolLink("User application 5", app5_url)
-                         )
-                        )
+def get():
+    return DivHStacked(
+            Img(src="wordslab-notebooks-small.jpg", width=96, height=96),
+            H1("Wordslab notebooks"),
+            Div("version 2025-04", cls=TextT.meta)
+        ), DivVStacked(
+            DivHStacked(
+                ToolCard("Open WebUI", "openwebui.jpg", openwebui_url),
+                ToolCard("Jupyter Lab", "jupyterlab.jpg", jupyterlab_url),
+                ToolCard("VS Code", "vscode.jpg", vscode_url),
+                Card(DivVStacked(
+                    H3("User applications"),
+                    Ol(
+                        ToolLink("USER_APP1_PORT", app1_url),
+                        ToolLink("USER_APP2_PORT", app2_url),
+                        ToolLink("USER_APP3_PORT", app3_url),
+                        ToolLink("USER_APP4_PORT", app4_url),
+                        ToolLink("USER_APP5_PORT", app5_url)
+                    )
+                )),
+                cls="space-x-10 my-10"
+            )           
+        )
+
+def ToolCard(name, image, url):
+    return Card(
+            A(DivVStacked(
+            H3(name),
+            Img(src=image, style="height:96px; width:96px; object-fit:contain"),
+            Span(url)
+            ), href=url, target="_blank")
+          )
 
 def ToolLink(name, url):
-    return Li(name + ": ", A(url, href=url, target="_blank"))
+    return Li(A(
+        DivHStacked(Span(name, cls=TextPresets.muted_sm), Span(url), cls="space-x-5"), 
+        href=url, target="_blank"), cls="my-1")
 
 serve(port=dashboard_port)
