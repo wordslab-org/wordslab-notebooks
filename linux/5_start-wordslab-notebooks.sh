14f164c50c49eb3092923be7e972df44f1a84bdf
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-05-08 15:12:34 +0200

    Update 5_start-wordslab-notebooks.sh

--- linux/5_start-wordslab-notebooks.sh
+++ linux/5_start-wordslab-notebooks.sh
@@ -63,7 +63,7 @@ jupyter lab -ServerApp.base_url="/" -ServerApp.ip=0.0.0.0 -ServerApp.port=$JUPYT
 pid2=$!
 
 # Start ollama server
-OLLAMA_HOME=0.0.0.0 OLLAMA_LOAD_TIMEOUT=-1 ollama serve &
+OLLAMA_HOST=0.0.0.0 OLLAMA_CONTEXT_LENGTH=8192 OLLAMA_LOAD_TIMEOUT=-1 ollama serve &
 pid3=$!
 
 # Start open-webui server
