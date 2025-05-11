ef3fa328098bf7ac404317dade7883fea9216c58
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-05-11 22:38:37 +0200

    Update all versions as of May 11 2025

--- linux/3_1_install-vscode-server.sh
+++ linux/3_1_install-vscode-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Visual Studio Code server in a persistent directory (see _wordslab-notebooks-env.bashrc)
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.99.3 --method standalone --prefix $VSCODE_DIR
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.99.4 --method standalone --prefix $VSCODE_DIR
 
 # Create the directory inside the workspace where all code-server state will be stored
 mkdir -p $VSCODE_DATA
