803fb4b879391dd88495a99915f9388d8f9f6b0a
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-05-30 17:27:44 +0200

    Split Ubuntu packages install in several lines : easier to diagnose what failed when the Ubuntu servers are down

--- linux/1_1_install-ubuntu-packages.sh
+++ linux/1_1_install-ubuntu-packages.sh
@@ -4,9 +4,14 @@ export DEBIAN_FRONTEND=noninteractive
 # Install essential commands to download and edit files, build and version code, monitor the machine, resolve network issues, and manage multimedia files
 
 apt update
-apt install --yes sudo locales ca-certificates curl wget unzip less vim tmux screen git git-lfs htop nvtop iputils-ping net-tools traceroute openssh-client build-essential cmake ffmpeg
-
+apt install --yes sudo locales
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
+apt install --yes less vim tmux screen
+apt install --yes ca-certificates curl wget unzip openssh-client
+apt-install --yes htop nvtop iputils-ping net-tools traceroute
+apt install --yes git git-lfs
+apt install --yes build-essential cmake
+apt install --yes ffmpeg
 
 # Uncomment the line below if you want to generate PDF / HTML versions of your notebooks
 # apt install fonts-liberation pandoc texlive-xetex texlive-fonts-recommended texlive-plain-generic
