0b363dcbc116ba36db5de1e1bcdad7190edb5179
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-08-16 18:47:45 +0200

    Update 2_5_install-datascience-libs.sh

--- linux/2_5_install-datascience-libs.sh
+++ linux/2_5_install-datascience-libs.sh
@@ -11,27 +11,22 @@ fi
 
 mkdir -p $WORDSLAB_MODELS
 
-if [ ! -f ~/.wordslab-installed ]; then
+echo '' >> ./_wordslab-notebooks-env.bashrc
+echo '# Models and datasets download directories' >> ./_wordslab-notebooks-env.bashrc
+echo 'export HF_HOME=$WORDSLAB_MODELS/huggingface' >> ./_wordslab-notebooks-env.bashrc
+echo 'export FASTAI_HOME=$WORDSLAB_MODELS/fastai' >> ./_wordslab-notebooks-env.bashrc
+echo 'export TORCH_HOME=$WORDSLAB_MODELS/torch' >> ./_wordslab-notebooks-env.bashrc
+echo 'export KERAS_HOME=$WORDSLAB_MODELS/keras' >> ./_wordslab-notebooks-env.bashrc
+echo 'export TFHUB_CACHE_DIR=$WORDSLAB_MODELS/tfhub_modules' >> ./_wordslab-notebooks-env.bashrc
+echo 'export OLLAMA_MODELS=$WORDSLAB_MODELS/ollama' >> ./_wordslab-notebooks-env.bashrc
 
-    echo '' >> ./_wordslab-notebooks-env.bashrc
-    echo '# Models and datasets download directories' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export HF_HOME=$WORDSLAB_MODELS/huggingface' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export FASTAI_HOME=$WORDSLAB_MODELS/fastai' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export TORCH_HOME=$WORDSLAB_MODELS/torch' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export KERAS_HOME=$WORDSLAB_MODELS/keras' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export TFHUB_CACHE_DIR=$WORDSLAB_MODELS/tfhub_modules' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export OLLAMA_MODELS=$WORDSLAB_MODELS/ollama' >> ./_wordslab-notebooks-env.bashrc
+# Replace the tutorials project with a brand new one when upgrading to a new wordslab version
+if [ -d $WORDSLAB_WORKSPACE/wordslab-notebooks-tutorials ]; then
+    rm -rf $WORDSLAB_WORKSPACE/wordslab-notebooks-tutorials
 fi
 
-if [ ! -d $WORDSLAB_WORKSPACE/wordslab-notebooks-tutorials ]; then
-
-    # Create a first workspace project with tutorials for wordslab notebooks
-    create-workspace-project https://github.com/wordslab-org/wordslab-notebooks-tutorials.git
-else
-
-    # Update the workspace project with tutorials for wordslab notebooks
-    git -C $WORDSLAB_WORKSPACE/wordslab-notebooks-tutorials pull
-fi
+# Create a workspace project with tutorials for wordslab notebooks
+create-workspace-project https://github.com/wordslab-org/wordslab-notebooks-tutorials.git
 
 # => create-workspace-project will copy the ../projects/pyproject.toml file in this workspace project
 # => then it will install all the common datascience librairies, Pytorch (GPU or CPU version), and VLLM (GPU version)
