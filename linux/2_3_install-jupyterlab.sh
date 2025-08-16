1bb9ade0b5af2f5900426831b24fffee74551d79
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-08-16 18:43:35 +0200

    Update 2_3_install-jupyterlab.sh

--- linux/2_3_install-jupyterlab.sh
+++ linux/2_3_install-jupyterlab.sh
@@ -6,18 +6,15 @@ mkdir -p $WORDSLAB_WORKSPACE
 # Also prepare a persistent directory to store the secrets used by all wordslab applications
 mkdir -p $WORDSLAB_WORKSPACE/.secrets
 
-if [ ! -f ~/.wordslab-installed ]; then
-
-    # Configure Jupyterlab to store all its state under the $WORDSLAB_WORKSPACE directory (see _wordslab-notebooks-env.bashrc)
-    echo '' >> ./_wordslab-notebooks-env.bashrc
-    echo '# Jupyterlab notebooks data directories' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export JUPYTER_CONFIG_DIR=$JUPYTER_DATA/etc/jupyter' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export JUPYTER_DATA_DIR=$JUPYTER_DATA/share/jupyter' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export JUPYTER_RUNTIME_DIR=$JUPYTER_DATA/share/jupyter/runtime' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export JUPYTERLAB_SETTINGS_DIR=$JUPYTER_DATA/lab/user-settings' >> ./_wordslab-notebooks-env.bashrc
-    echo 'export JUPYTERLAB_WORKSPACES_DIR=$JUPYTER_DATA/lab/workspaces' >> ./_wordslab-notebooks-env.bashrc
-fi
-
+# Configure Jupyterlab to store all its state under the $WORDSLAB_WORKSPACE directory (see _wordslab-notebooks-env.bashrc)
+echo '' >> ./_wordslab-notebooks-env.bashrc
+echo '# Jupyterlab notebooks data directories' >> ./_wordslab-notebooks-env.bashrc
+echo 'export JUPYTER_CONFIG_DIR=$JUPYTER_DATA/etc/jupyter' >> ./_wordslab-notebooks-env.bashrc
+echo 'export JUPYTER_DATA_DIR=$JUPYTER_DATA/share/jupyter' >> ./_wordslab-notebooks-env.bashrc
+echo 'export JUPYTER_RUNTIME_DIR=$JUPYTER_DATA/share/jupyter/runtime' >> ./_wordslab-notebooks-env.bashrc
+echo 'export JUPYTERLAB_SETTINGS_DIR=$JUPYTER_DATA/lab/user-settings' >> ./_wordslab-notebooks-env.bashrc
+echo 'export JUPYTERLAB_WORKSPACES_DIR=$JUPYTER_DATA/lab/workspaces' >> ./_wordslab-notebooks-env.bashrc
+    
 # Create jupterlab environment
 mkdir -p $JUPYTERLAB_ENV
 cp ./2_3_jupyterlab-pyproject.toml $JUPYTERLAB_ENV/pyproject.toml
