5bdeed9f068dfaf4b7e44d4246ff159049cea19f
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-08-16 12:32:57 +0200

    Update 3_1_install-vscode-server.sh

--- linux/3_1_install-vscode-server.sh
+++ linux/3_1_install-vscode-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Visual Studio Code server in a persistent directory (see _wordslab-notebooks-env.bashrc)
-curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.103.0 --method standalone --prefix $VSCODE_DIR
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --version 4.103.1 --method standalone --prefix $VSCODE_DIR
 
 # Create the directory inside the workspace where all code-server state will be stored
 mkdir -p $VSCODE_DATA
@@ -10,4 +10,4 @@ mkdir -p $VSCODE_DATA
 $VSCODE_DIR/bin/code-server --install-extension ms-python.python@2025.4.0 --extensions-dir $VSCODE_DATA/extensions
 
 # Install the Continue AI code assistant
-CONTINUE_GLOBAL_DIR=$VSCODE_DATA/.continue $VSCODE_DIR/bin/code-server --install-extension Continue.continue@1.0.22 --extensions-dir $VSCODE_DATA/extensions
+CONTINUE_GLOBAL_DIR=$VSCODE_DATA/.continue $VSCODE_DIR/bin/code-server --install-extension Continue.continue@1.0.23 --extensions-dir $VSCODE_DATA/extensions
