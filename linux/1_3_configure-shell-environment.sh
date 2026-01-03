df16d534e9cfc4cc94fe7e3f6f5b4ccd840bd44a
Laurent Prudhon <laurent.prudhon@hotmail.fr>
2026-01-03 13:29:32 +0100

    Add user environment variables file

--- linux/1_3_configure-shell-environment.sh
+++ linux/1_3_configure-shell-environment.sh
@@ -2,6 +2,9 @@
 
 # NB: this script can only be run after 2__install_wordslab-notebooks.sh AND 3__install-open-webui.sh
 
+# Prepare a script which will store the API keys and other environment variables saved by the user
+touch $WORDSLAB_WORKSPACE/.secrets/user-env.bashrc
+
 # Source _wordslab-notebooks.bashrc in ~/.bashrc
 # - set environment variables for storage and ports
 # - start a new shell in the right Python environment and the right directory
@@ -10,6 +13,7 @@
 # => the last version overwrites the previous values
 echo '' >> ~/.bashrc
 echo source $WORDSLAB_SCRIPTS/linux/_wordslab-notebooks-env.bashrc >> ~/.bashrc
+echo source $WORDSLAB_WORKSPACE/.secrets/user-env.bashrc >> ~/.bashrc
 echo source $WORDSLAB_SCRIPTS/linux/_wordslab-notebooks-init.bashrc >> ~/.bashrc
 
 # Add a flag file to ditinguish the first install from the following updates
