7cbe1eceae1374826086eb83822ab115441f983f
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2026-01-14 22:32:48 +0100

    Add zstd package - used to uncompress ollama download

--- linux/1_1_install-ubuntu-packages.sh
+++ linux/1_1_install-ubuntu-packages.sh
@@ -7,7 +7,7 @@ apt update
 apt install --yes sudo apt-utils locales 
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
 apt install --yes less vim nano tmux screen
-apt install --yes ca-certificates curl wget unzip openssh-client
+apt install --yes ca-certificates curl wget unzip zstd openssh-client
 apt install --yes htop nvtop iputils-ping net-tools traceroute
 apt install --yes git git-lfs
 apt install --yes build-essential cmake
