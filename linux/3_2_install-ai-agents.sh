f6f6392e31aec24c192578862f5cb5d62433a9e5
Laurent Prudhon <laurent.prudhon@hotmail.fr>
2026-02-01 10:25:21 +0100

    Last versions updates for version 2026-01

--- linux/3_2_install-ai-agents.sh
+++ linux/3_2_install-ai-agents.sh
@@ -2,16 +2,16 @@
 
 # https://docs.cline.bot/getting-started/installing-cline#vscodium%2Fwindsurf
 
-$VSCODE_DIR/bin/code-server --install-extension saoudrizwan.claude-dev@3.53.1 --extensions-dir $VSCODE_DATA/extensions
+$VSCODE_DIR/bin/code-server --install-extension saoudrizwan.claude-dev@3.56.2 --extensions-dir $VSCODE_DATA/extensions
 
 # https://kilo.ai/docs/getting-started/installing#open-vsx-registry
 
-$VSCODE_DIR/bin/code-server --install-extension kilocode.kilo-code@4.152.0 --extensions-dir $VSCODE_DATA/extensions
+$VSCODE_DIR/bin/code-server --install-extension kilocode.kilo-code@5.2.2 --extensions-dir $VSCODE_DATA/extensions
 
 # https://github.com/mistralai/mistral-vibe?tab=readme-ov-file#using-uv
 
-uv tool install mistral-vibe==1.3.5
+uv tool install mistral-vibe==2.0.2
 
 # https://opencode.ai/
 
-curl -fsSL https://opencode.ai/install | bash -s -- --version 1.1.34
+curl -fsSL https://opencode.ai/install | bash -s -- --version 1.1.48
