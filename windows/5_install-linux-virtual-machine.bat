12e135b57aadeb3e6ed1e7c77fe0cc386096818d
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2026-05-09 18:39:41 +0200

    Adjust to the new security context : can't acces windows files

--- windows/5_install-linux-virtual-machine.bat
+++ windows/5_install-linux-virtual-machine.bat
@@ -1,13 +1,6 @@
 REM Usage : 5_install-linux-virtual-machine <wsl distribution name>(required) <cpu only?>(required)
 
-rem Capture the current directory
-set "originalDir=%cd%"
-
 REM Note : you can't change WORDSLAB_WORKSPACE or WORDSLAB_MODELS default paths in the WSL virtual machine
 REM => this would break 4_mount-linux-virtual-disks.bat
 
-cd ..
-wsl -d %1 -- WORDSLAB_VERSION=%WORDSLAB_VERSION% ./install-wordslab-notebooks.sh %2
-
-rem Return to the original directory
-cd "%originalDir%"
+wsl -d %1 -- bash -lc "export WORDSLAB_VERSION=%WORDSLAB_VERSION%; apt update && apt install -y curl unzip && curl -fsSL https://raw.githubusercontent.com/wordslab-org/wordslab-notebooks/refs/tags/%WORDSLAB_VERSION%/install-wordslab-notebooks.sh | bash -s -- %2"
